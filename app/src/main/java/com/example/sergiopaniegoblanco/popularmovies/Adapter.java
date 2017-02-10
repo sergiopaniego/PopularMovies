5a713a5cda1d5b3cbbf70ddc342b430dd41b37de
sergiopaniego <sergiopaniegoblanco@gmail.com>
2017-02-10 18:07:36 +0100

    RecycledView added and Adapter initiated

---
+++ app/src/main/java/com/example/sergiopaniegoblanco/popularmovies/Adapter.java
@@ -0,0 +1,85 @@
+package com.example.sergiopaniegoblanco.popularmovies;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by sergiopaniegoblanco on 10/02/2017.
+ */
+
+public class Adapter extends RecyclerView.Adapter<Adapter.PosterViewHolder>{
+    private int mNumberItems;
+    Context context;
+    public Adapter(int numberOfItems) {
+        mNumberItems = numberOfItems;
+    }
+
+    @Override
+    public PosterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context=parent.getContext();
+        int layoutIdForListItem = R.layout.poster_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        PosterViewHolder viewHolder = new PosterViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(PosterViewHolder holder, int position) {
+        Log.d(TAG, "#" + position);
+        Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").error(R.drawable.fff)
+                .into(holder.listItemNumberView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+
+    /**
+     * Cache of the children views for a list item.
+     */
+    class PosterViewHolder extends RecyclerView.ViewHolder {
+
+        // Will display the position in the list, ie 0 through getItemCount() - 1
+        ImageView listItemNumberView;
+
+        /**
+         * Constructor for our ViewHolder. Within this constructor, we get a reference to our
+         * TextViews and set an onClickListener to listen for clicks. Those will be handled in the
+         * onClick method below.
+         * @param itemView The View that you inflated in
+         *                 {@link Adapter#onCreateViewHolder(ViewGroup, int)}
+         */
+        public PosterViewHolder(View itemView) {
+            super(itemView);
+
+            listItemNumberView = (ImageView) itemView.findViewById(R.id.poster_item);
+        }
+
+        /**
+         * A method we wrote for convenience. This method will take an integer as input and
+         * use that integer to display the appropriate text within a list item.
+         * @param listIndex Position of the item in the list
+         */
+        void bind(Drawable listIndex) {
+            listItemNumberView.setImageDrawable(listIndex);
+        }
+    }
+}
--- app/src/main/java/com/example/sergiopaniegoblanco/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/sergiopaniegoblanco/popularmovies/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.sergiopaniegoblanco.popularmovies;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView mNumbersList;
+    private static final int NUM_LIST_ITEMS=100;
+    private Adapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNumbersList = (RecyclerView) findViewById(R.id.recycledview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mNumbersList.setLayoutManager(layoutManager);
+
+        /*
+         * Use this setting to improve performance if you know that changes in content do not
+         * change the child layout size in the RecyclerView
+         */
+        mNumbersList.setHasFixedSize(true);
+
+        /*
+         * The GreenAdapter is responsible for displaying each item in the list.
+         */
+        mAdapter = new Adapter(NUM_LIST_ITEMS);
+
+        mNumbersList.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected=item.getItemId();
+        if(menuItemThatWasSelected==R.id.action_search&&item.getTitle()=="SORT BY TITLE"){
+            Context context=MainActivity.this;
+            String message="Item clicked";
+            Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+
+            item.setTitle("SORT BY RELEVANCE");
+        }else{
+            item.setTitle("SORT BY TITLE");
+            Context context=MainActivity.this;
+            String message="Item clicked";
+            Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
